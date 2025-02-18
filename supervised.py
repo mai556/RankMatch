4e91bc7879f666a81e64f4787449b6ced316df05
mai556 <50618245+mai556@users.noreply.github.com>
2025-02-18 11:02:05 +0800

    Update supervised.py

--- supervised.py
+++ supervised.py
@@ -21,7 +21,7 @@ from util.utils import count_params, AverageMeter, intersectionAndUnion, init_lo
 from util.dist_helper import setup_distributed
 
 
-parser = argparse.ArgumentParser(description='Revisiting Weak-to-Strong Consistency in Semi-Supervised Semantic Segmentation')
+parser = argparse.ArgumentParser(description='RankMatch: Exploring the Better Consistency Regularization for Semi-supervised Semantic Segmentation')
 parser.add_argument('--config', type=str, required=True)
 parser.add_argument('--labeled-id-path', type=str, required=True)
 parser.add_argument('--unlabeled-id-path', type=str, default=None)
